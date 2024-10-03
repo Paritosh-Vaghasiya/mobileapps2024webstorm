6cbf5760550f6dbeb9c11789b7aec455f8ce16cf
VIPCodeCoder <vaghasiya1923@students.d211.org>
2024-10-03 07:50:16 -0500

    error fix

--- auth.js
+++ auth.js
@@ -1,31 +1,35 @@
-const {createClient} = window.supabase;
+const { createClient } = window.supabase;
 
 const supabaseUrl = "https://egzhuriimugvkjiauphl.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnemh1cmlpbXVndmtqaWF1cGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwNzEzNjcsImV4cCI6MjAzOTY0NzM2N30.29e4s0hYCEB3e4m0GDB2WgSpEDbiJSSC4FOg5aU8ZOk";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnemh1cmlpbXVndmtqaWF1cGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwNzEzNjcsImV4cCI6MjAzOTY0NzM2N30.29e4s0hYCEB3e4m0GDB2WgSpEDbiJSSC4FOg5aU8ZOk";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const loginBtn = document.getElementById("loginBtn");
 loginBtn?.addEventListener("click", async () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const{error, session} = await supabase.auth.signInWithPassword({email, password});
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const { error, session } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
 
-    if(error){
-        document.getElementById("error-msg").textContent = error.message;
-    } else {
-        window.location.href = 'display.html';
-    }
+  if (error) {
+    document.getElementById("error-msg").textContent = error.message;
+  } else {
+    window.location.href = "display.html";
+  }
 });
 
 //Signup
 const signupBtn = document.getElementById("signupBtn");
 signupBtn?.addEventListener("click", async () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const city = document.getElementById("city").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const city = document.getElementById("city").value;
 
   const { error: signUpError, user } = await supabase.auth.signUp({
     email,
@@ -34,20 +38,20 @@ signupBtn?.addEventListener("click", async () => {
 
   if (signUpError) {
     document.getElementById("error-msg").textContent = signUpError.message;
-  }
-
-  const { error: insertError } = await supabase.from("Table_1").insert([
-    {
-      firstName: firstName,
-      lastName: lastName,
-      city: city,
-      email: email,
-    },
-  ]);
-
-  if (insertError) {
-    document.getElementById("error-msg").textContent = insertError.message;
   } else {
-    window.location.href = "index.html";
+    const { error: insertError } = await supabase.from("Table_1").insert([
+      {
+        firstName: firstName,
+        lastName: lastName,
+        city: city,
+        email: email,
+      },
+    ]);
+
+    if (insertError) {
+      document.getElementById("error-msg").textContent = insertError.message;
+    } else {
+      window.location.href = "index.html";
+    }
   }
 });
